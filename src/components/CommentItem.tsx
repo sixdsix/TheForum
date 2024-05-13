bfad93fb9411bfad3cca96fb425550c3c6a9b7fb
Nathan Miller <112665760+sixdsix@users.noreply.github.com>
2024-05-12 22:57:27 -0700

    Update CommentItem.tsx

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -1,4 +1,4 @@
-const CommentItem = ({ comment }: { comment: CommentType }) => {
+const CommentItem = ({ comment }) => {
     return (
         <div style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '10px', borderRadius: '5px' }}>
             <p>{comment.content}</p>
